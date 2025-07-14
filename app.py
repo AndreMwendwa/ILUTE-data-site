446e5ab3a1da92296ce037a4217c0e4b52ab9843
AndreMwendwa <mwendwakiko@gmail.com>
2025-07-14 11:13:35 -0400

    Refs to great_tables lib removed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from streamlit_extras.great_tables import great_tables
-from great_tables import GT
+# from streamlit_extras.great_tables import great_tables
+# from great_tables import GT
 from Utils.utils import get_years, make_html_table_from_dataframe, replace_semicolon_with_linebreak, get_all_column_values
 
 
@@ -36,7 +36,7 @@ class dashboard:
         self.selected_tags = st.sidebar.multiselect("Select Tags", tags, default=tags)
 
     def table(self):
-        from great_tables import GT
+        # from great_tables import GT
         # st.subheader("Dataset Overview")
         self.filtered_data = self.data.copy()
         if self.years: 
@@ -53,41 +53,12 @@ class dashboard:
             self.filtered_data = self.filtered_data[
                 self.filtered_data['Tags'].apply(lambda x: any(tag in x for tag in self.selected_tags))
             ]
-        table = (
-            GT(self.filtered_data)
-            .tab_header("Dataset Overview")
-                 )
+        # table = (
+            # GT(self.filtered_data)
+            # .tab_header("Dataset Overview")
+            #     )
         # great_tables(table, width="content")
         st.markdown(make_html_table_from_dataframe(self.filtered_data), unsafe_allow_html=True)
-        
-    def example():
-        try:
-            from great_tables import GT
-            from great_tables.data import sp500
-
-            # Define the start and end dates for the data range
-            start_date = "2010-06-07"
-            end_date = "2010-06-14"
-
-            # Filter sp500 using Pandas to dates between `start_date` and `end_date`
-            sp500_mini = sp500[(sp500["date"] >= start_date) & (sp500["date"] <= end_date)]
-
-            # Create a display table based on the `sp500_mini` table data
-            table = (
-                GT(sp500_mini)
-                .tab_header(title="S&P 500", subtitle=f"{start_date} to {end_date}")
-                .fmt_currency(columns=["open", "high", "low", "close"])
-                .fmt_date(columns="date", date_style="wd_m_day_year")
-                .fmt_number(columns="volume", compact=True)
-                .cols_hide(columns="adj_close")
-            )
-
-            great_tables(table, width="stretch")
-        except ImportError:
-            st.warning(
-                "This example requires the `great_tables` package. "
-                "Install it with `pip install great-tables`."
-            )
 
 
     def render(self):
