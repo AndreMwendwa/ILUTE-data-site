e7c3564e3b7744af6d1290d6e7d969193d18d54b
AndreMwendwa <mwendwakiko@gmail.com>
2025-07-22 22:38:48 -0400

    Map size modified

--- pages/3_land_use.py
+++ pages/3_land_use.py
@@ -3,4 +3,4 @@ import pandas as pd
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://jz-ilute.github.io/LandUseSample/", height=1000)
+components.iframe("https://jz-ilute.github.io/LandUseSample/", height=750)
--- pages/4_transport.py
+++ pages/4_transport.py
@@ -3,4 +3,4 @@ import pandas as pd
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-components.iframe("https://jz-ilute.github.io/TransportSamples/", height=1000)
+components.iframe("https://jz-ilute.github.io/TransportSamples/", height=750)
