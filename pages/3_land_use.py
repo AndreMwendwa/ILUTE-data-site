6aba286746d2db47a1fd261cec44a403a4001f1e
AndreMwendwa <mwendwakiko@gmail.com>
2025-07-22 22:29:41 -0400

    Maps added

---
+++ pages/3_land_use.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+components.iframe("https://jz-ilute.github.io/LandUseSample/", height=1000)
---
+++ pages/4_transport.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+components.iframe("https://jz-ilute.github.io/TransportSamples/", height=1000)
